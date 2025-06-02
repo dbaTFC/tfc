eea500d310b54222a95f6c05209ccd8bc8958cfd
dani <daniel@example.com>
2025-06-02 20:03:09 +0200

    cambios websocket

--- src/main/java/com/tfg/gestionproyectos/config/WebSocketConfig.java
+++ src/main/java/com/tfg/gestionproyectos/config/WebSocketConfig.java
@@ -8,18 +8,16 @@ import org.springframework.web.socket.config.annotation.*;
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    // Endpoint donde los clientes se conectarán vía WebSocket
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("https://tfc-t00f.onrender.com/ws-chat") // URL a la que se conectará el frontend
-                .setAllowedOriginPatterns("*") // Permitir todos los orígenes (ajustable para producción)
-                .withSockJS(); // Compatibilidad con navegadores antiguos
+        registry.addEndpoint("/ws-chat")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
     }
 
-    // Configurar el broker de mensajes interno (in-memory)
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic"); // prefijo de salida (mensajes del servidor al cliente)
-        config.setApplicationDestinationPrefixes("/app"); // prefijo para los mensajes que vienen del cliente
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
     }
 }
