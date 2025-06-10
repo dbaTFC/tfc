8a14361e23a1ce722f330c07e398cfb397d8bbea
dbaTFC <danielbarberoalarcon@gmail.com>
2025-06-10 18:34:28 +0200

    cambios en SecurityCOnfig

--- src/main/java/com/tfg/gestionproyectos/security/SecurityConfig.java
+++ src/main/java/com/tfg/gestionproyectos/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -67,7 +68,7 @@ public class SecurityConfig {
                 "/swagger-ui.html",
                 "/login"
             ).permitAll()
-
+            .requestMatchers(HttpMethod.POST, "/miembros").permitAll()
             // Toddos los endpoints requieren autenticación, pero no se limita por rol global
             .anyRequest().authenticated()
 )
